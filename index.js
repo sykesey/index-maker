a0ef84eaf5a49294e77310f22ff403da9f43ad03
Ben Sykes <benpsykes@gmail.com>
2020-03-20 09:02:22 +1100

    Update formatting and comments

--- index.js
+++ index.js
@@ -26,26 +26,29 @@ let keywords = {};
 for (var i = 0; i < lines.length; i++)
 {
    let currentLine = lines[i].toLowerCase();
+   //don't bother with  lines that only contain blank spaces
    if (currentLine.match(/^ +$/))
      continue;
+
    //explode into keywords by space
    let lineKeywords = currentLine.split(/ +/);
+
    //go into each keyword and collate into keyword buckets
    for (var j = 0; j < lineKeywords.length; j++)
    {
       let currentKeyword = lineKeywords[j];
+      //don't bother with empty keywords
       if (currentKeyword.match(/^ +$/) || currentKeyword == "")
         continue;
-      //keywords[ lineKeywords[j] ]++;
+      //now put them into buckets and increase the count if already found
       if ( keywords.hasOwnProperty(currentKeyword) )
          keywords [ currentKeyword ] += 1
       else
-	 keywords [ currentKeyword ] = 1
+	       keywords [ currentKeyword ] = 1
    }
 }
 //now sort them all in reverse order before we print them out
-
 let keywordsSorted = Object.keys(keywords).sort(function(a,b){return keywords[b]-keywords[a]})
 
-//sort here?
+//print them all out now using forEach on the array
 keywordsSorted.forEach( i => process.stdout.write( i + " " + keywords[i] + "\n" ) );
